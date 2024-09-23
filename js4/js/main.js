7f3ede0c051930b665046372c772faed9afd6574
Sudip Subedi <88761244+Isudipsubedi@users.noreply.github.com>
2024-09-23 16:53:36 +0545

    Math functions

---
+++ js4/js/main.js
@@ -0,0 +1,22 @@
+ // Math Methods and Propertues
+ console.log(Math.PI);
+ console.log(Math.trunc(Math.PI));
+ console.log(Math.round(3.1));
+ console.log(Math.ceil(3.1));
+ console.log(Math.floor(3.8));
+
+ //pow
+ console.log(Math.pow(2,5));
+
+ console.log(Math.min(3,0.5,8));
+
+ console.log(Math.max(3,0.5,8));
+
+ //random
+ console.log(Math.random());
+// from 1 to 10
+console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
